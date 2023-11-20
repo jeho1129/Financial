fa32452cf5a7f22cc18cbd5ff5da5c984fe69176
jeho1129 <jeho1129@gmail.com>
2023-11-20 14:09:27 +0900

    BACK_ADMINUSER

--- back/banks/urls.py
+++ back/banks/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('deposits/<str:fin_prdt_cd>/', views.detail_deposits),
     path('deposits/<str:fin_prdt_cd>/comments/', views.comment_deposits),
     path('deposits/comments/<int:comment_pk>/', views.comment_deposits_detail),
+    path('deposits/change/<str:fin_prdt_cd>/', views.change_deposits),
     path('savings/save/', views.save_savings),
     path('savings/list/', views.list_savings),
     path('savings/<str:fin_prdt_cd>/', views.detail_savings),
