08087a30ed69f92bb46fe88b703665f253a83aad
jeho1129 <jeho1129@naver.com>
2023-11-20 10:28:25 +0900

    BACK_API

--- back/banks/views.py
+++ back/banks/views.py
@@ -21,39 +21,42 @@ from .serializers import DepositProductsSerializer, DepositOptionsSerializer, De
 @permission_classes([IsAuthenticatedOrReadOnly])
 def save_deposits(request):
     API_KEY = settings.API_KEY
-    url = f'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth={API_KEY}&topFinGrpNo=020000&pageNo=1'
-    response = requests.get(url).json()
-    for li in response.get("result").get("baseList"):
-        save_data = {
-            'fin_co_no' : li.get('fin_co_no'),
-            'kor_co_nm' : li.get('kor_co_nm'),
-            'fin_prdt_cd' : li.get('fin_prdt_cd'),
-            'fin_prdt_nm' : li.get('fin_prdt_nm'),
-            'join_way' : li.get('join_way'),
-            'join_member' : li.get('join_member'),
-            'spcl_cnd' : li.get('spcl_cnd'),
-            'join_deny' : li.get('join_deny')
-        }
+    request_parameters = [('020000', 1), ('030300', 4)]  # topFinGrpNo와 pageNo를 튜플로 지정
+    for topFinGrpNo, maxPageNo in request_parameters:
+        for pageNo in range(1, maxPageNo + 1):
+            url = f'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth={API_KEY}&topFinGrpNo={topFinGrpNo}&pageNo={pageNo}'
+            response = requests.get(url).json()
+            for li in response.get("result").get("baseList"):
+                save_data = {
+                    'fin_co_no' : li.get('fin_co_no'),
+                    'kor_co_nm' : li.get('kor_co_nm'),
+                    'fin_prdt_cd' : li.get('fin_prdt_cd'),
+                    'fin_prdt_nm' : li.get('fin_prdt_nm'),
+                    'join_way' : li.get('join_way'),
+                    'join_member' : li.get('join_member'),
+                    'spcl_cnd' : li.get('spcl_cnd'),
+                    'join_deny' : li.get('join_deny')
+                }
 
-        if not DepositProducts.objects.filter(fin_prdt_cd=save_data['fin_prdt_cd']).exists():
-            serializer = DepositProductsSerializer(data=save_data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
+                if not DepositProducts.objects.filter(fin_prdt_cd=save_data['fin_prdt_cd']).exists():
+                    serializer = DepositProductsSerializer(data=save_data)
+                    if serializer.is_valid(raise_exception=True):
+                        serializer.save()
 
-    for li in response.get("result").get("optionList"):
-        save_data = {
-            'fin_prdt_cd' : li.get('fin_prdt_cd'),
-            'intr_rate_type_nm' : li.get('intr_rate_type_nm'),
-            'intr_rate' : li.get('intr_rate'),
-            'intr_rate2' : li.get('intr_rate2'),
-            'save_trm' : li.get('save_trm')
-        }
+            for li in response.get("result").get("optionList"):
+                save_data = {
+                    'fin_prdt_cd' : li.get('fin_prdt_cd'),
+                    'intr_rate_type_nm' : li.get('intr_rate_type_nm'),
+                    'intr_rate' : li.get('intr_rate'),
+                    'intr_rate2' : li.get('intr_rate2'),
+                    'save_trm' : li.get('save_trm')
+                }
 
-        if not DepositOptions.objects.filter(fin_prdt_cd=save_data['fin_prdt_cd'], intr_rate_type_nm=save_data['intr_rate_type_nm'], intr_rate=save_data['intr_rate'], intr_rate2=save_data['intr_rate2'], save_trm=save_data['save_trm']).exists():
-            product = DepositProducts.objects.get(fin_prdt_cd=save_data['fin_prdt_cd'])
-            serializer = DepositOptionsSerializer(data=save_data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save(product=product)
+                if not DepositOptions.objects.filter(fin_prdt_cd=save_data['fin_prdt_cd'], intr_rate_type_nm=save_data['intr_rate_type_nm'], intr_rate=save_data['intr_rate'], intr_rate2=save_data['intr_rate2'], save_trm=save_data['save_trm']).exists():
+                    product = DepositProducts.objects.get(fin_prdt_cd=save_data['fin_prdt_cd'])
+                    serializer = DepositOptionsSerializer(data=save_data)
+                    if serializer.is_valid(raise_exception=True):
+                        serializer.save(product=product)
     return Response({ 'message': 'okay' }, status=status.HTTP_200_OK)
 
 
@@ -103,39 +106,43 @@ def comment_deposits_detail(request, comment_pk):
 @permission_classes([IsAdminUser])
 def save_savings(request):
     API_KEY = settings.API_KEY
-    url = f'http://finlife.fss.or.kr/finlifeapi/savingProductsSearch.json?auth={API_KEY}&topFinGrpNo=020000&pageNo=1'
-    response = requests.get(url).json()
-    for li in response.get("result").get("baseList"):
-        save_data = {
-            'fin_co_no' : li.get('fin_co_no'),
-            'kor_co_nm' : li.get('kor_co_nm'),
-            'fin_prdt_cd' : li.get('fin_prdt_cd'),
-            'fin_prdt_nm' : li.get('fin_prdt_nm'),
-            'join_way' : li.get('join_way'),
-            'join_member' : li.get('join_member'),
-            'spcl_cnd' : li.get('spcl_cnd'),
-            'join_deny' : li.get('join_deny')
-        }
+    request_parameters = [('020000', 1), ('030300', 3)]
 
-        if not SavingProducts.objects.filter(fin_prdt_cd=save_data['fin_prdt_cd']).exists():
-            serializer = SavingProductsSerializer(data=save_data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
+    for topFinGrpNo, maxPageNo in request_parameters:
+        for pageNo in range(1, maxPageNo + 1):
+            url = f'http://finlife.fss.or.kr/finlifeapi/savingProductsSearch.json?auth={API_KEY}&topFinGrpNo={topFinGrpNo}&pageNo={pageNo}'
+            response = requests.get(url).json()
+            for li in response.get("result").get("baseList"):
+                save_data = {
+                    'fin_co_no' : li.get('fin_co_no'),
+                    'kor_co_nm' : li.get('kor_co_nm'),
+                    'fin_prdt_cd' : li.get('fin_prdt_cd'),
+                    'fin_prdt_nm' : li.get('fin_prdt_nm'),
+                    'join_way' : li.get('join_way'),
+                    'join_member' : li.get('join_member'),
+                    'spcl_cnd' : li.get('spcl_cnd'),
+                    'join_deny' : li.get('join_deny')
+                }
 
-    for li in response.get("result").get("optionList"):
-        save_data = {
-            'fin_prdt_cd' : li.get('fin_prdt_cd'),
-            'intr_rate_type_nm' : li.get('intr_rate_type_nm'),
-            'intr_rate' : li.get('intr_rate'),
-            'intr_rate2' : li.get('intr_rate2'),
-            'save_trm' : li.get('save_trm')
-        }
+                if not SavingProducts.objects.filter(fin_prdt_cd=save_data['fin_prdt_cd']).exists():
+                    serializer = SavingProductsSerializer(data=save_data)
+                    if serializer.is_valid(raise_exception=True):
+                        serializer.save()
 
-        if not SavingOptions.objects.filter(fin_prdt_cd=save_data['fin_prdt_cd'], intr_rate_type_nm=save_data['intr_rate_type_nm'], intr_rate=save_data['intr_rate'], intr_rate2=save_data['intr_rate2'], save_trm=save_data['save_trm']).exists():
-            product = SavingProducts.objects.get(fin_prdt_cd=save_data['fin_prdt_cd'])
-            serializer = SavingOptionsSerializer(data=save_data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save(product=product)
+            for li in response.get("result").get("optionList"):
+                save_data = {
+                    'fin_prdt_cd' : li.get('fin_prdt_cd'),
+                    'intr_rate_type_nm' : li.get('intr_rate_type_nm'),
+                    'intr_rate' : li.get('intr_rate'),
+                    'intr_rate2' : li.get('intr_rate2'),
+                    'save_trm' : li.get('save_trm')
+                }
+
+                if not SavingOptions.objects.filter(fin_prdt_cd=save_data['fin_prdt_cd'], intr_rate_type_nm=save_data['intr_rate_type_nm'], intr_rate=save_data['intr_rate'], intr_rate2=save_data['intr_rate2'], save_trm=save_data['save_trm']).exists():
+                    product = SavingProducts.objects.get(fin_prdt_cd=save_data['fin_prdt_cd'])
+                    serializer = SavingOptionsSerializer(data=save_data)
+                    if serializer.is_valid(raise_exception=True):
+                        serializer.save(product=product)
     return Response({ 'message': 'okay' }, status=status.HTTP_200_OK)
 
 
@@ -182,7 +189,7 @@ def comment_savings_detail(request, comment_pk):
 
 
 @api_view(['GET'])
-@permission_classes([IsAdminUser])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def exchanges(request):
     API_KEY = settings.API_KEY_2
     url = f'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={API_KEY}&data=AP01'
@@ -195,17 +202,16 @@ def saving_dummy_reviews(request):
     user_count = get_user_model().objects.count()
     product_count = SavingProducts.objects.count()
 
-    # 더미 데이터를 생성합니다.
-    for _ in range(1000):  # 10,000개의 더미 데이터를 생성합니다.
-        # 랜덤한 사용자와 상품을 선택합니다.
+    for _ in range(10000):
+        # 랜덤한 사용자와 상품을 선택
         random_user = get_user_model().objects.all()[random.randint(0, user_count - 1)]
         random_product = SavingProducts.objects.all()[random.randint(0, product_count - 1)]
     
-        # 랜덤한 평점과 내용을 생성합니다.
+        # 랜덤한 평점과 내용을 생성
         random_rating = random.randint(1, 5)  # 1부터 5까지의 랜덤한 정수
         random_content = lorem.text()  # 랜덤한 텍스트
     
-        # SavingReviews 객체를 생성하고 데이터베이스에 저장합니다.
+        # SavingReviews 객체를 생성하고 데이터베이스에 저장
         review = SavingReviews(product=random_product, user=random_user, rating=random_rating, content=random_content)
         review.save()
 
@@ -217,6 +223,7 @@ item_similarity_df = None
 
 @api_view(['GET'])
 def saving_rating_matrix(request):
+    # 사용자와 상품에 대한 평점 행렬을 생성
     reviews = SavingReviews.objects.all()
     df = read_frame(reviews, fieldnames=['user__id', 'product__id', 'rating'])
     df = df.rename(columns={'user__id': 'user_id', 'product__id': 'product_id'})
@@ -226,6 +233,8 @@ def saving_rating_matrix(request):
         columns='product_id',
         values='rating'
     )
+    # 상품 간의 Cosine 유사도 계산
+    # 코사인 유사도는 두 벡터 간의 코사인 각을 사용하여 측정한 유사도로, 값이 1에 가까울수록 유사도가 높다는 것을 의미
     item_similarity = cosine_similarity(rating_matrix.T.fillna(0))
     item_similarity_df = pd.DataFrame(
         item_similarity,
@@ -234,28 +243,35 @@ def saving_rating_matrix(request):
     )
     return Response(item_similarity_df.to_dict())
 
+# Item-Based Collaborative Filtering Algorithm
+# 사용자의 행동 패턴(아이템에 대한 평점)을 분석하여, 사용자가 아직 평가하지 않은 아이템 중에 사용자가 선호할 만한 아이템을 찾아내는 방법이다.
+# 아이템 간의 유사도를 계산하여, 사용자가 이미 평가한 아이템과 유사도가 높은 아이템을 추천한다.
+# 사용자가 아직 아무런 아이템도 평가하지 않았거나, 모든 아이템의 유사도가 0일 경우에는 인기도가 높은 아이템을 추천하는 로직을 포함
+# 새로운 사용자나 데이터가 부족한 사용자에게 추천을 제공하는 방법인 콜드 스타트 문제를 해결함.
 
 @api_view(['GET'])
 def saving_recommend_items(request, user_pk, item_numbers):
     item_similarity_dict = saving_rating_matrix(request._request).data
     item_similarity_df = pd.DataFrame.from_dict(item_similarity_dict)
-    # user_pk를 가진 사용자의 리뷰를 가져옴
+    # user_pk를 가진 사용자의 리뷰를 가져와 사용자가 평가한 상품의 ID를 리스트로 생성
     user_reviews = SavingReviews.objects.filter(user__id=user_pk)
-    print(item_similarity_df)
     user_product_ids = [review.product_id for review in user_reviews]
     
     # user_product_ids가 DataFrame의 인덱스에 존재하는지 확인
+    # 사용자가 평가한 상품이 없다면 가장 인기가 많은 아이템을 추천함.
     if not set(user_product_ids).intersection(set(item_similarity_df.index)):
         popular_items = item_similarity_df.sum().sort_values(ascending=False)
         recommended_items = popular_items.index[:item_numbers]
         return Response({'recommended_items': recommended_items.tolist()})
 
+    # 사용자가 평가한 상품이 있다면, 해당 상품에 대한 유사도를 평균내어 사용자의 평점을 계산
     user_ratings = item_similarity_df.loc[user_product_ids].mean().dropna()
     sorted_user_ratings = user_ratings.sort_values(ascending=False)
+    # 사용자가 평가한 모든 아이템의 유사도가 0인 경우에는 가장 인기 있는 아이템을 추천
     if user_ratings.sum() == 0:
-        # 유사도 대신 인기도 높은 아이템 추천 또는 랜덤하게 선택
         popular_items = item_similarity_df.sum().sort_values(ascending=False)
         recommended_items = popular_items.index[:item_numbers]
+    # 가장 높은 평점을 받은 상품과 유사한 상품을 찾아 추천
     else:
         item_similarity_df = item_similarity_df.drop(user_product_ids, errors='ignore')
         top_item_id = sorted_user_ratings.index[0]
