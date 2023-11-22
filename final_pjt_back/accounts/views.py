94792c3057b86cbc614131794d845ee7128dbce8
jeho1129 <jeho1129@naver.com>
2023-11-22 15:19:20 +0900

    BACK 비밀번호 변경 로직 추가

--- final_pjt_back/accounts/views.py
+++ final_pjt_back/accounts/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import UserInfoSerializer, UserChangeSerializer
 
 
@@ -18,6 +19,12 @@ def userinfo(request):
     elif request.method == 'PUT':
         serializer = UserChangeSerializer(user, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
+            if 'password1' in request.data:
+                if request.data['password1'] == request.data['password2']:
+                    user.set_password(request.data['password1'])
+                    update_session_auth_hash(request, user)
+                else:
+                    return Response({'message': '비밀번호가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         
